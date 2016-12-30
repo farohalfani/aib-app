cb0f20194598b3b970624ab5110daffbbf9356e4
farohalfani <farohnuralfani@gmail.com>
2016-12-30 07:58:15 +0700

    test register

---
+++ NEWLoginRegister-master/Register.php
@@ -0,0 +1,17 @@
+<?php
+    $con = mysqli_connect("http://farohnura.000webhostapp.com/", "id425706_registerdbs", "registerdb", "id425706_registerdbs");
+    
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $statement = mysqli_prepare($con, "INSERT INTO user (name, username, age, password) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "ssis", $name, $username, $age, $password);
+    mysqli_stmt_execute($statement);
+    
+    $response = array();
+    $response["success"] = true;  
+    
+    echo json_encode($response);
+?>
